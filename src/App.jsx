8629084ee9e246106b38057cee97b064496d7a6c
niaaz <geniaaz1@protonmail.com>
2025-01-28 23:18:17 +0800

    tut-section/react-jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const title = 'React'; // only defined once, not everytime component rendered
+const welcome = {
+  title: 'Hey',
+  greeting: 'React'
+}; // only defined once, not everytime component rendered
+
+function getContent() {
+  return 'I\'m just a little boy from a little town'
+}
+
+const arr = ['one', 'two', 'Three0'];
+const transformedArr = Array.from(arr).map(item => item.toUpperCase());
 
 function App() {
   return (
       <div>
-        <h1>Hello React</h1>
+        <h1>{welcome.title} {welcome.greeting}</h1>
+        <body>{getContent()} {transformedArr.join(' ')}</body>
+
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"/>
       </div>
   )
 }
